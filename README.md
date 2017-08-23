169fd237f1044ceef3ce64ad5c827946a182a239
Yuhan Fang <yuhan.fang@gmail.com>
2017-08-23 10:57:51 -0400

    Use ID function instead of ID column.
    Fixes #1.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # bq
 Google Bigquery wrappers
 
+See http://godoc.org/github.com/ggqq/bq for full documentation.
+
 WARNING: API is currently unstable.
--- insert_structs.go
+++ insert_structs.go
@@ -10,14 +10,24 @@ import (
 )
 
 // InsertStructs uses the given bigquery client to insert the slice of
-// uniformly-typed structs to the given table in the given dataset. The field
-// corresponding to the given ID is used to de-duplicate insertions. If the
-// dataset and/or table are missing, then they will be created.
-func InsertStructs(ctx context.Context, client *bigquery.Client, dataset, table, id string, structs interface{}) error {
+// uniformly-typed structs to the given table in the given dataset. If the
+// dataset and/or table are missing, then they will be created. The ider
+// function must take a struct from the passed slice of structs, and return a
+// string that uniquely identifies that element. The ID is used for
+// de-duplication.
+func InsertStructs(ctx context.Context, client *bigquery.Client, dataset, table string, ider interface{}, structs interface{}) error {
 	rv := reflect.ValueOf(structs)
 	if rv.Kind() != reflect.Slice {
 		return fmt.Errorf("InsertStructs can insert slice of structs; got %v", rv.Type())
 	}
+	fv := reflect.ValueOf(ider)
+	if fv.Kind() != reflection || fv.Type().NumIn() != 1 || fv.Type().NumOut() != 1 || fv.Type().Out(0) != reflect.TypeOf("") ||
+		(fv.Type().In(0) != rv.Elem().Type() &&
+			(fv.Type().In(0).Kind() == reflect.Interface &&
+				!rv.Elem().Type().Implements(fv.Type().In(0)))) {
+		return fmt.Errorf("InsertStructs requires an ider that takes %v and returns string; got %v", rv.Elem().Type(), fv.Type())
+	}
+
 	if rv.Len() == 0 {
 		return nil
 	}
@@ -26,7 +36,7 @@ func InsertStructs(ctx context.Context, client *bigquery.Client, dataset, table,
 	if err != nil {
 		return err
 	}
-	MakeOptional(schema, id)
+	MakeOptional(schema)
 
 	ds := client.Dataset(dataset)
 	ds.Create(ctx) // Ignore error, which is returned if the dataset already exists.
@@ -48,18 +58,12 @@ func InsertStructs(ctx context.Context, client *bigquery.Client, dataset, table,
 	savers := []*bigquery.StructSaver{}
 	for i := 0; i < rv.Len(); i++ {
 		val := rv.Index(i).Interface()
-		insertIDGeneric, ok := reflection.StructFieldIgnoreCase(val, id)
-		if !ok {
-			return fmt.Errorf("InsertStructs: value %v does not have field %s", val, id)
-		}
-		insertID, ok := insertIDGeneric.(string)
-		if !ok {
-			return fmt.Errorf("InsertStructs: value %v does not have string field %s", val, id)
-		}
+		id := fv.Call([]reflect.Value{rv.Index(i)})[0].Interface().(string)
+
 		savers = append(savers, &bigquery.StructSaver{
 			Struct:   val,
 			Schema:   schema,
-			InsertID: insertID,
+			InsertID: id,
 		})
 	}
 	return ul.Put(ctx, savers)
